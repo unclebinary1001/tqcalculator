b8b4e426322dfbd11f6280d0290750a267e05231
Mahlangu <nzundamahlangu@gmail.com>
2024-04-13 03:46:46 -0500

    add hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,40 @@
+import {
+  Button,
+  Container,
+  Flex,
+  Image,
+  Space,
+  Text,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
+import HeroImg from "../../public/hero_img.png";
+
+export function Hero() {
+  const theme = useMantineTheme();
+  return (
+    <Flex justify={"space-between"} gap={"sm"}>
+      <Image
+        src={HeroImg}
+        alt={"Mantine hero image"}
+        h={400}
+        w="auto"
+        fit="contain"
+      />
+      <Container style={{ margin: "auto", width: 600 }}>
+        <Title order={2} size={32} style={{ color: theme.colors.brand[0], letterSpacing: 1 }}>
+          Enhance Your Engineering Potential
+        </Title>
+        <Space h={30} />
+
+        <Text size="lg">
+          We make engineering economy fun, so that you are ready to achieve your
+          goals.
+        </Text>
+        <Space h={30} />
+
+        <Button radius="xl" color={theme.colors.brand[5]}>Get Started</Button>
+      </Container>
+    </Flex>
+  );
+}
