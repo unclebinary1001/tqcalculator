b59a488f571374a10cfdfeb0da5b9859bf472e4f
Mahlangu <nzundamahlangu@gmail.com>
2024-04-17 03:18:15 -0500

    add Fa and Af factors

---
+++ src/components/factors/Fa.tsx
@@ -0,0 +1,157 @@
+import {
+  Container,
+  Grid,
+  Title,
+  Button,
+  useMantineTheme,
+  Text,
+  NumberInput,
+  Group,
+  Box,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import formatDecimals from "../../utils/formatDecimals";
+
+export default function Fa() {
+  const theme = useMantineTheme();
+  const navigate = useNavigate();
+
+  const [ans, setAns] = useState({
+    factor: "0",
+    futureValue: "0",
+  }); 
+
+  const calculateAns = () => {
+    const { A, i, n } = form.values;
+    const rate = i/100;
+    const fa = (Math.pow(1 + rate,n) - 1)/ rate;
+    const future = A * fa;
+    setAns({ factor: formatDecimals(fa, 4), futureValue: (A === 0)? "0": formatDecimals(future, 2) });
+  }
+
+  const resetAll = () => {
+    form.setValues({ A: 0, i: 0, n: 0 });
+    setAns({ factor: "0", futureValue: "0" });
+  }
+
+  
+  const form = useForm({
+    initialValues: {
+      A: 0,
+      i: 0,
+      n: 0,
+    },
+    validate: {
+      // F: (value) => (value > 0 ? null : 'Future worth must be greater than 0'),
+      i: (value) => (value > 0 ? null : 'Interest must be greater than 0'),
+      n: (value) => (value > 0 ? null : 'N must be greater than 0'),
+    },
+  });
+  return (
+    <Container p={40}>
+      <Title ta={"center"} order={2} fw={600} mb={8}>
+        Convert A to F
+      </Title>
+      <form
+        onSubmit={form.onSubmit(() => {
+          calculateAns();
+        })}
+      >
+        <Grid>
+          <Grid.Col span={4}>
+            <NumberInput
+              label="Amount A"
+              placeholder="Dollars"
+              prefix="$"
+              hideControls
+              defaultValue={form.values.A}
+              {...form.getInputProps('A')}
+              mb={"md"}
+            />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <NumberInput
+              label="Interest Rate i %"
+              placeholder="i%"
+              hideControls
+              defaultValue={form.values.A}
+              {...form.getInputProps('i')}
+            />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <NumberInput
+              label="No. of periods n"
+              placeholder="N"
+              hideControls
+              defaultValue={form.values.A}
+              {...form.getInputProps('n')}
+              mb="md"
+            />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Title order={6} size={"md"} fw={600}>
+              F/A factor:
+            </Title>
+            <Box
+              px={8}
+              py={4}
+              mb="md"
+              style={{
+                borderRadius: 8,
+                border: "1px solid",
+                borderColor: theme.colors.brand[2],
+                height: '2em'
+              }}
+            >
+              <Text size={"md"}>{ans.factor == "0"? "": ans.factor}</Text>
+            </Box>
+           
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Title order={6} size={"md"} fw={600}>
+              Future F:
+            </Title>
+            <Box
+              px={8}
+              py={4}
+              mb="md"
+              style={{
+                borderRadius: 8,
+                border: "1px solid",
+                borderColor: theme.colors.brand[2],
+                height: '2em'
+              }}
+            >
+              <Text size={"md"}>{ans.futureValue == "0"? "": "$" + ans.futureValue}</Text>
+            </Box>
+            </Grid.Col>
+        </Grid>
+
+        <Group justify="flex-start" mt="md">
+          <Button
+            type="submit"
+            radius="xl"
+            size="sm"
+            color={form.isValid() == true? theme.colors.brand[5]: theme.colors.brand[11]}
+            
+          >
+            <Text>Solve</Text>
+          </Button>
+          <Button radius="xl" size="sm" color={theme.colors.brand[13]} onClick={() => resetAll()}>
+            <Text>Reset</Text>
+          </Button>
+          <Button
+            radius="xl"
+            size="sm"
+            color={theme.colors.brand[12]}
+            onClick={() => navigate(-1)}
+          >
+            <Text>Back</Text>
+          </Button>
+        </Group>
+      </form>
+    </Container>
+  );
+}
