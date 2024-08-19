ab4dc2a847918ff2a959aa91a55c66a1624aacb5
Mahlangu <nzundamahlangu@gmail.com>
2024-08-19 15:21:56 -0500

    add api call to open router LLM

--- src/components/CashFlowDiagram.tsx
+++ src/components/CashFlowDiagram.tsx
@@ -10,10 +10,42 @@ import {
 import { useState } from "react";
 
 function limitWordCount(str: string, wordLimit: number): string {
-    const words = str.split(" ").filter(Boolean);
-    const limitedWords = words.slice(0, wordLimit);
-    return limitedWords.join(' ');
+  const words = str.split(" ").filter(Boolean);
+  const limitedWords = words.slice(0, wordLimit);
+  return limitedWords.join(" ");
+}
+
+const promptLLM = async (statement: string) => {
+  try {
+    const response = await fetch(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_OPENROUTER_API_KEY}`,
+          "HTTP-Referer": `${import.meta.env.VITE_SITE_URL}`, // Optional, for including your app on openrouter.ai rankings.
+          "X-Title": `${import.meta.env.VITE_SITE_NAME}`, // Optional. Shows in rankings on openrouter.ai.
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "meta-llama/llama-3.1-8b-instruct:free",
+          messages: [{ role: "user", content: "What is the meaning of life?" }],
+        }),
+      }
+    );
+    console.log("Call LLM")
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json)
+    // TODO: load message content from LLM
+    // console.log(json['choices'][0]['message']['content']);
+  } catch (error) {
+    console.error("Error:", error);
   }
+};
 
 function CashFlowDiagram() {
   const theme = useMantineTheme();
@@ -29,8 +61,8 @@ function CashFlowDiagram() {
     if (wordCount <= WORD_LIMIT) {
       setStatement(newValue);
     } else {
-        const limitedValue = limitWordCount(newValue, WORD_LIMIT);
-        setStatement(limitedValue);
+      const limitedValue = limitWordCount(newValue, WORD_LIMIT);
+      setStatement(limitedValue);
     }
   };
   return (
@@ -39,26 +71,32 @@ function CashFlowDiagram() {
         placeholder="Enter your cash flow statement here"
         label="Cash Flow Statement"
         value={statement}
+        size={"md"}
         onChange={(e) => handleStatementChange(e)}
         minRows={4}
         maxRows={7}
         autosize
         required
+        styles={{ input: { color: "#808080" } }}
       />
-      <Group pos={"relative"} mt="xs">
+      <Group justify="flex-end" mt="xs">
         <Text size="sm" style={{ color: theme.colors.brand[6] }}>
           {statement.split(/\s+/).filter(Boolean).length} / {WORD_LIMIT} words
         </Text>
       </Group>
-      <Space h="lg" />
-      <Button
-        radius="xl"
-        variant="filled"
-        color={theme.colors.brand[4]}
-        mb={"md"}
-      >
-        Generate Diagram
-      </Button>
+      <Space h="xl" />
+      <Group justify="center" mt="md">
+        <Button
+          radius="xl"
+          variant="filled"
+          color={theme.colors.brand[9]}
+          mb={"md"}
+          disabled={statement.length === 0}
+          onClick={() => {promptLLM(statement)}}
+        >
+          Generate Diagram
+        </Button>
+      </Group>
     </Container>
   );
 }
