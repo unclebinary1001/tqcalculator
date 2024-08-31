fd83491657a28b9ea2cc36346f8c1ffa61881140
Mahlangu <nzundamahlangu@gmail.com>
2024-08-30 20:58:57 -0500

    improve formatting of amount and rendering of chart

--- src/components/CashFlowDigram/CustomLineChart.tsx
+++ src/components/CashFlowDigram/CustomLineChart.tsx
@@ -17,23 +17,19 @@ const CustomLineChart: React.FC<CustomLineChartProps> = ({ data }) => {
   const maxValue = Math.max(...values);
 
   const width = 800;
-  const trueHeight = 600;
+  const trueWidth = 900;
+  const trueHeight = 550;
   const height = 300;
-  const margin = { top: 20, right: 20, bottom: 40, left: 50 };
+  const margin = { top: 30, right: 20, bottom: 40, left: 60 };
 
   const scaleY = (amount: number) => {
-    return (
-        amount > 0? (
-      height -
-      margin.bottom -
-      ((amount - 0 ) / (maxValue)) *
-        (height - margin.top - margin.bottom) ): (
-            height -
-            margin.bottom +
-            ((amount - 0) / (minValue)) *
-              (height - margin.top - margin.bottom) 
-        )
-    );
+    return amount > 0
+      ? height -
+          margin.bottom -
+          ((amount - 0) / maxValue) * (height - margin.top - margin.bottom)
+      : height -
+          margin.bottom +
+          ((amount - 0) / minValue) * (height - margin.top - margin.bottom);
   };
 
   const scaleX = (index: number) => {
@@ -45,8 +41,14 @@ const CustomLineChart: React.FC<CustomLineChartProps> = ({ data }) => {
     return values[index] > 0 ? "up" : "down";
   };
 
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+
   return (
-    <svg width={width} height={trueHeight}>
+    <svg width={trueWidth} height={trueHeight}>
       <line
         x1={margin.left}
         y1={height - margin.bottom}
@@ -71,7 +73,7 @@ const CustomLineChart: React.FC<CustomLineChartProps> = ({ data }) => {
 
         return (
           <React.Fragment key={index}>
-            {(
+            {
               <line
                 x1={x}
                 y1={height - margin.bottom}
@@ -80,30 +82,39 @@ const CustomLineChart: React.FC<CustomLineChartProps> = ({ data }) => {
                 stroke="blue"
                 strokeWidth={2}
               />
-            )}
+            }
             {direction === "up" && (
-              <text
-                x={x}
-                y={y}
-                textAnchor="middle"
-                fill="blue"
-                fontSize="16"
-              >
-                ▲
-              </text>
+              <>
+                <text x={x} y={y} textAnchor="middle" fill="blue" fontSize="16">
+                  ▲
+                </text>
+                <text
+                  x={x}
+                  y={y - 10}
+                  textAnchor="middle"
+                  fill="blue"
+                  fontSize="16"
+                >
+                  {formatter.format(point.amount)}
+                </text>
+              </>
             )}
             {direction === "down" && (
-              <text
-                x={x}
-                y={y}
-                textAnchor="middle"
-                fill="blue"
-                fontSize="16"
-              >
-                ▼
-              </text>
+              <>
+                <text x={x} y={y} textAnchor="middle" fill="blue" fontSize="16">
+                  ▼
+                </text>
+                <text
+                  x={x}
+                  y={y + 10}
+                  textAnchor="middle"
+                  fill="blue"
+                  fontSize="16"
+                >
+                  {formatter.format(Math.abs(point.amount))}
+                </text>
+              </>
             )}
-            {/* Render the year labels */}
             <text
               x={x}
               y={height - 25}
@@ -112,20 +123,26 @@ const CustomLineChart: React.FC<CustomLineChartProps> = ({ data }) => {
               fill="black"
             >
               {point.year}
-            </text> 
+            </text>
           </React.Fragment>
         );
       })}
 
-      <text x={width / 2} y={height - 5} textAnchor="middle" fontSize="16" fontWeight={"semi-bold"}>
+      <text
+        x={width / 2}
+        y={height - 5}
+        textAnchor="middle"
+        fontSize="16"
+        fontWeight={"semi-bold"}
+      >
         Year
       </text>
       <text
-        x={-height / 2}
+        x={-height + margin.bottom}
         y={margin.right}
         textAnchor="middle"
         transform={`rotate(-90, 0, 0)`}
-        fontSize="14"
+        fontSize="16"
       >
         $ Amount
       </text>
